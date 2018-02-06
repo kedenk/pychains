95b1655c1b229f4b88a13a1d81d1aae5b2507a92
Rahul Gopinath <rahul@gopinath.org>
2018-02-06 14:31:00 +0100

    better pudb experience

--- gencmd.py
+++ gencmd.py
@@ -1,6 +1,6 @@
 import pudb
 import os
-if os.getenv('D'): pudb.set_trace()
+if os.getenv('D'): pudb.set_trace(paused=False)
 import random
 if os.getenv('R'): random.seed(int(os.getenv('R')))
 mi = int(os.getenv('I')) if os.getenv('I') else 100000

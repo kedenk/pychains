3eb90ed543369a15792b4aea7eddd4645af0e84f
Rahul Gopinath <rahul@gopinath.org>
2018-02-09 12:14:24 +0100

    lower maxiter

--- gencmd.py
+++ gencmd.py
@@ -3,7 +3,7 @@ import os
 if os.getenv('D'): pudb.set_trace(paused=True)
 import random
 if os.getenv('R'): random.seed(int(os.getenv('R')))
-mi = int(os.getenv('I')) if os.getenv('I') else 100000
+mi = int(os.getenv('I')) if os.getenv('I') else 10000
 import pygen.execfile
 pygen.execfile.set_maxiter(mi)
 if os.getenv('D'): pygen.execfile.set_debug(int(os.getenv('D')))

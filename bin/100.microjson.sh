7386f2ad6f712fdc3a1caa72420d1766b1a8340f
Rahul Gopinath <rahul@gopinath.org>
2018-02-21 10:59:20 +0100

    better trap

--- bin/100.microjson.sh
+++ bin/100.microjson.sh
@@ -1,3 +1,3 @@
 start=0$1
-#(for i in `seq 1 100`; do echo $i; echo --------; sleep 1; env R=$i P=C python3 gencmd.py subjects/microjson.py; done ) 2>microjson.err | tee microjson.log
-(for i in `seq $start 100`; do echo $i --------; sleep 1; env R=$i P=U python3 gencmd.py subjects/microjson.py; done ) 2>microjson.err | tee microjson.log
+(for i in `seq $start 1000`; do echo $i --------; sleep 1; env R=$i P=U python3 gencmd.py subjects/microjson.py; done ) 2>microjson.err | tee microjson.log
+trap times EXIT
--- bin/100.urlparser.sh
+++ bin/100.urlparser.sh
@@ -1,2 +1,3 @@
 start=0$1
 (for i in `seq $start 100`; do echo $i; sleep 1; echo --------; sleep 1; env R=$i ISTRATEGY=0.1 P=C python3 gencmd.py subjects/urlparser.py; done ) 2>&1 | tee urlparser.log
+trap times EXIT

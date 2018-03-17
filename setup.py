a11754dc3145f13a5099952e95efaf0b7447c66b
Rahul Gopinath <rahul@gopinath.org>
2018-03-17 19:40:36 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='PyChains',
+    version='1.0',
+    description='Test Generator',
+    author='Rahul Gopinath',
+    author_email='rahul@gopinath.org',
+    packages=['pychains'],
+    )

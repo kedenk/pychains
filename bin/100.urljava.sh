7382d556da026b54dc536c8337afd06e81d8838e
Rahul Gopinath <rahul@gopinath.org>
2018-03-19 11:45:45 +0100

    urljava

--- bin/100.urljava.sh
+++ bin/100.urljava.sh
@@ -1,2 +1,12 @@
-start=0$1
-(for i in `seq $start 100`; do echo $i; sleep 1; echo --------; sleep 1; env R=$i python3 pychains/chain.py subjects/urljava.py; done ) 2>&1 | tee url.log
+#!/usr/bin/env bash
+SLEEP=0
+if [[ $1 == '-' ]]
+then
+  echo env R=$2 P=U python3 $D pychains/chain.py subjects/urljava.py
+  env R=$2 P=U python3 $D pychains/chain.py subjects/urljava.py
+else
+  start=0$1
+  (for i in `seq $start 1000`; do echo $i --------; sleep $SLEEP; env R=$i P=U python3 pychains/chain.py subjects/urljava.py; done ) 2>urljava.err | tee urljava.log
+fi
+
+trap times EXIT

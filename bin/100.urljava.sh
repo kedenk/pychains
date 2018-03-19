3da95ef32919f15df1e9cdc40cf353e8e3845d6f
Rahul Gopinath <rahul@gopinath.org>
2018-03-19 18:55:39 +0100

    urljava

--- bin/100.urljava.sh
+++ bin/100.urljava.sh
@@ -2,11 +2,11 @@
 SLEEP=0
 if [[ $1 == '-' ]]
 then
-  echo env R=$2 P=U python3 $D pychains/chain.py subjects/urljava.py
-  env R=$2 P=U python3 $D pychains/chain.py subjects/urljava.py
+  echo env R=$2 P=U python3 $D ./bin/mychains.py subjects/urljava.py
+  env R=$2 P=U python3 $D ./bin/mychains.py subjects/urljava.py
 else
   start=0$1
-  (for i in `seq $start 1000`; do echo $i --------; sleep $SLEEP; env R=$i P=U python3 pychains/chain.py subjects/urljava.py; done ) 2>urljava.err | tee urljava.log
+  (for i in `seq $start 1000`; do echo $i --------; sleep $SLEEP; env MY_RP=0.01 R=$i P=U python3 bin/mychains.py subjects/urljava.py; done ) 2>urljava.err | tee urljava.log
 fi
 
 trap times EXIT

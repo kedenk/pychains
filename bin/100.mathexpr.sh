6f12cd1bd0696a347403e25b6933d050c865c4a6
Rahul Gopinath <rahul@gopinath.org>
2018-03-20 16:32:05 +0100

    mathexpr

--- bin/100.mathexpr.sh
+++ bin/100.mathexpr.sh
@@ -2,11 +2,11 @@
 SLEEP=0
 if [[ $1 == '-' ]]
 then
-  echo env R=$2 P=U BFS=true python3 $D ./bin/mychains.py subjects/mathexpr.py
-  env R=$2 P=U BFS=true python3 $D ./bin/mychains.py subjects/mathexpr.py
+  echo env R=$2 P=U python3 $D ./bin/mychains.py subjects/mathexpr.py
+  env R=$2 P=U python3 $D ./bin/mychains.py subjects/mathexpr.py
 else
   start=0$1
-  (for i in `seq $start 1000`; do echo $i --------; sleep $SLEEP; env BFS=true R=$i P=U python3 bin/mychains.py subjects/mathexpr.py; done ) 2>mathexpr.err | tee mathexpr.log
+  (for i in `seq $start 1000`; do echo $i --------; sleep $SLEEP; env R=$i P=U python3 bin/mychains.py subjects/mathexpr.py; done ) 2>mathexpr.err | tee mathexpr.log
 fi
 
 trap times EXIT

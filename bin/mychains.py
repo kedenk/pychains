c07d7cb6c92e0fc3ad9ae4a63cdd3c57c046e855
Rahul Gopinath <rahul@gopinath.org>
2018-04-12 07:57:55 -0700

    mychains

--- bin/mychains.py
+++ bin/mychains.py
@@ -2,10 +2,14 @@
 import sys
 import pychains.chain
 import imp
+import taintedstr
 if __name__ == "__main__":
     arg = sys.argv[1]
+    times = int(sys.argv[2]) if len(sys.argv) > 2 else 1
     _mod = imp.load_source('mymod', arg)
-    e = pychains.chain.Chain()
-    (a, r) = e.exec_argument(_mod.main)
-    print("Arg:", repr(a))
-    print("Eval:", repr(r))
+    results = []
+    for i in range(times):
+        e = pychains.chain.Chain()
+        (a, r) = e.exec_argument(_mod.main)
+        print(a, flush=True)
+        taintedstr.reset_comparisons()
